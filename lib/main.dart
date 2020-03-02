7406e6dab551d311fac9026663acea18a3aea6e6
nischal tiwari <nikks068@gmail.com>
2020-03-02 17:06:52 +0545

    managed filter screen and did state life cycle

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:foodi_app/dummy_data.dart';
+import 'package:foodi_app/models/meal.dart';
 import 'package:foodi_app/screens/bottom_bar.dart';
 import 'package:foodi_app/screens/category_meal_screen.dart';
 import 'package:foodi_app/screens/category_overview_screen.dart';
 import 'package:foodi_app/screens/filter_screen.dart';
 import 'package:foodi_app/screens/meal_details_screen.dart';
-import 'package:foodi_app/screens/tab_view_screen.dart';
+//import 'package:foodi_app/screens/tab_view_screen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filter = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  void _setFilter(Map<String, bool> filterData) {
+    setState(() {
+      _filter = filterData;
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filter['gluten'] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filter['lactose'] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filter['vegetarian'] && !meal.isVegetarian) {
+          return false;
+        }
+        if (_filter['vegan'] && !meal.isVegan) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,10 +66,11 @@ class MyApp extends StatelessWidget {
       ),
       home: BottomBarScreen(),
       routes: {
-        CategoryMealScreen.routeName: (ctx) => CategoryMealScreen(),
+        CategoryMealScreen.routeName: (ctx) =>
+            CategoryMealScreen(_availableMeals),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
         BottomBarScreen.routeName: (ctx) => BottomBarScreen(),
-        FilterScreen.routeName: (ctx) => FilterScreen(),
+        FilterScreen.routeName: (ctx) => FilterScreen(_setFilter, _filter),
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(builder: (ctx) => CategoryOverviewScreen());
--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -1,16 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:foodi_app/widgets/main_drawer.dart';
 
-class FilterScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
   static const String routeName = '/filter_screen';
+  final Function setFilter;
+  final Map<String, bool> currentFilter;
+  FilterScreen(this.setFilter, this.currentFilter);
+
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  bool _glutenFree = false;
+  bool _vegan = false;
+  bool _vegetarian = false;
+  bool _LactoseFree = false;
+
+  Widget _buildSwitchListTile(String title, String description,
+      bool currentValue, Function updateValue) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue,
+    );
+  }
+
+  @override
+  void initState() {
+    _glutenFree = widget.currentFilter['gluten'];
+    _vegan = widget.currentFilter['vegan'];
+    _vegetarian = widget.currentFilter['vegetarian'];
+    _LactoseFree = widget.currentFilter['lactose'];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Filters'),
-      ),
-      drawer: MainDrawer(),
-    );
+        appBar: AppBar(
+          title: Text('Filters'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                final filterData = {
+                  'gluten': _glutenFree,
+                  'lactose': _LactoseFree,
+                  'vegan': _vegan,
+                  'vegetarian': _vegetarian,
+                };
+                widget.setFilter(filterData);
+              },
+            )
+          ],
+        ),
+        drawer: MainDrawer(),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Adjust your filters here!',
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  _buildSwitchListTile(
+                      'Gluten Free',
+                      'Only Gluten free meals are selected',
+                      _glutenFree, (value) {
+                    setState(() {
+                      _glutenFree = value;
+                    });
+                  }),
+                  _buildSwitchListTile(
+                      'Lactose Free',
+                      'Only Lactose free meals are selected',
+                      _LactoseFree, (value) {
+                    setState(() {
+                      _LactoseFree = value;
+                    });
+                  }),
+                  _buildSwitchListTile(
+                      'Vegan', 'Only Vegan meals are selected', _vegan,
+                      (value) {
+                    setState(() {
+                      _vegan = value;
+                    });
+                  }),
+                  _buildSwitchListTile(
+                      'vageterian',
+                      'Only vageterian meals are selected',
+                      _vegetarian, (value) {
+                    setState(() {
+                      _vegetarian = value;
+                    });
+                  }),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
